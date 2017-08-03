80a5d8f6d21d39a2fd174e0e21ff8b4e8dc8495e
Marcel Singer <masinger@climobi047.intern.lise.de>
2017-08-03 19:11:00 +0200

    Initial commit

---
+++ RandomCodeOrg.MvExtension/NameBuilder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RandomCodeOrg.MvExtension
+{
+    public class NameBuilder
+    {
+
+        public string BuildName(string prefix, string suffix) {
+            if (string.IsNullOrEmpty(prefix) && string.IsNullOrEmpty(suffix))
+                return "";
+            if (string.IsNullOrEmpty(prefix))
+                return suffix;
+            if (string.IsNullOrEmpty(suffix))
+                return prefix;
+            if (prefix.EndsWith(".") || suffix.StartsWith(".") || prefix.EndsWith("]") || suffix.StartsWith("["))
+                return $"{prefix}{suffix}";
+            return $"{prefix}.{suffix}";
+        }
+
+    }
+}
---
+++ RandomcodeOrg.MvExtension.Core/PartialExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Html;
+using System;
+
+namespace RandomCodeOrg.MvExtension {
+    public static class PartialExtensions {
+        
+        public static HtmlString PartialFor(this TagHelper) {
+            return new HtmlString("");
+        }
+
+    }
+}
